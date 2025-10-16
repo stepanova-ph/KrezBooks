ccf010ade9b96cca21ff9743e5f6a062513f8cf2
Petra <ph.stepanova@gmail.com>
2025-10-16 16:05:49 +0200

    d

--- src/renderer/components/common/FormDialog.tsx
+++ src/renderer/components/common/FormDialog.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {
   Dialog,
-  DialogTitle,
   DialogContent,
   DialogActions,
   Button,
+  IconButton,
+  Box,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface FormDialogProps {
   open: boolean;
@@ -38,10 +40,50 @@ export function FormDialog({
         sx: {
           maxWidth: 600,
           width: "100%",
+          borderRadius: 0, // No rounded corners like main window
         },
       }}
     >
-      <DialogTitle sx={{ pb: 1 }}>{title}</DialogTitle>
+      {/* Blue header matching AppBar */}
+      <Box
+        sx={{
+          width: "100%",
+          height: 48,
+          backgroundColor: "#00556B", // Same as AppBar
+          display: "flex",
+          alignItems: "center",
+          padding: "0 16px",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            color: "#FFF",
+            fontWeight: 600,
+            fontSize: "1rem",
+          }}
+        >
+          {title}
+        </Box>
+
+        {/* Close button - copied from WindowControls */}
+        <IconButton
+          size="small"
+          onClick={onClose}
+          sx={{
+            color: "#FFF",
+            borderRadius: 0,
+            height: 32,
+            width: 32,
+            "&:hover": {
+              backgroundColor: (theme) => theme.palette.error.main,
+            },
+          }}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </Box>
+
       <form onSubmit={onSubmit} noValidate>
         <DialogContent
           sx={{ display: "flex", flexDirection: "column", gap: 1.5, p: 2 }}
