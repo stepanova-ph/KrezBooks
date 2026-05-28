62a8a92cdd63f203274c8892b3c23318a2de0ed7
stepanova-ph <95447434+stepanova-ph@users.noreply.github.com>
2026-05-28 11:18:02 +0200

    refactor: restructure party boxes with ICO/DIC first, separators between groups, EUR-conditional IBAN/BIC

--- src/templates/invoice/invoiceRenderer.ts
+++ src/templates/invoice/invoiceRenderer.ts
@@ -78,33 +78,28 @@ function generatePageHeader(
           <h3 class="party-title">Dodavatel</h3>
           <div class="party-content">
             <div class="party-group">
-              <p class="party-name">${seller.ownerName}</p>
-              <p class="party-name">${seller.companyName}</p>
               <table class="party-details"><tbody>
                 <tr><td class="party-label">IČO:</td><td>${seller.ico}</td></tr>
                 ${seller.dic ? `<tr><td class="party-label">DIČ:</td><td>${seller.dic}</td></tr>` : ""}
               </tbody></table>
+              <p class="party-name">${seller.ownerName}</p>
+              <p class="party-name">${seller.companyName}</p>
               <p>${seller.street}</p>
               <p>${seller.city}, ${seller.postalCode}</p>
             </div>
             <div class="party-group">
               <table class="party-details"><tbody>
-                ${invoice.is_in_eur ? "" : `<tr><td class="party-label">Účet:</td><td>${seller.bankAccount}</td></tr>`}
+                <tr><td class="party-label">Účet:</td><td>${seller.bankAccount}</td></tr>
+                ${invoice.is_in_eur && seller.iban ? `<tr><td class="party-label">IBAN:</td><td>${seller.iban}</td></tr>` : ""}
+                ${invoice.is_in_eur && seller.bic ? `<tr><td class="party-label">BIC:</td><td>${seller.bic}</td></tr>` : ""}
+              </tbody></table>
+            </div>
+            <div class="party-group">
+              <table class="party-details"><tbody>
                 <tr><td class="party-label">Tel:</td><td>${seller.phone}${seller.phone2 ? ` / ${seller.phone2}` : ""}</td></tr>
                 <tr><td class="party-label">Email:</td><td>${seller.email}</td></tr>
               </tbody></table>
             </div>
-            ${invoice.is_in_eur ? `
-            <div class="party-group">
-              <p class="party-group-label">Bankovní spojení:</p>
-              <table class="party-details party-details-indented"><tbody>
-                <tr><td class="party-label">Číslo účtu:</td><td>${seller.bankAccount}</td></tr>
-                ${seller.bankName ? `<tr><td class="party-label">Název banky:</td><td>${seller.bankName}</td></tr>` : ""}
-                ${seller.iban ? `<tr><td class="party-label">IBAN:</td><td>${seller.iban}</td></tr>` : ""}
-                ${seller.bic ? `<tr><td class="party-label">BIC:</td><td>${seller.bic}</td></tr>` : ""}
-              </tbody></table>
-            </div>
-            ` : ""}
           </div>
         </div>
 
@@ -112,30 +107,28 @@ function generatePageHeader(
           <h3 class="party-title">Odběratel</h3>
           <div class="party-content">
             <div class="party-group">
-              <p class="party-name">${buyer.companyName}</p>
               ${buyer.ico || buyer.dic ? `
               <table class="party-details"><tbody>
                 ${buyer.ico ? `<tr><td class="party-label">IČO:</td><td>${buyer.ico}</td></tr>` : ""}
                 ${buyer.dic ? `<tr><td class="party-label">DIČ:</td><td>${buyer.dic}</td></tr>` : ""}
               </tbody></table>
               ` : ""}
+              <p class="party-name">${buyer.companyName}</p>
               ${buyer.street ? `<p>${buyer.street}</p>` : ""}
               ${buyer.city && buyer.postalCode ? `<p>${buyer.city}, ${buyer.postalCode}</p>` : ""}
             </div>
-            ${buyer.bankAccount || buyer.phone || buyer.email ? `
+            ${buyer.bankAccount ? `
             <div class="party-group">
               <table class="party-details"><tbody>
-                ${!invoice.is_in_eur && buyer.bankAccount ? `<tr><td class="party-label">Účet:</td><td>${buyer.bankAccount}</td></tr>` : ""}
-                ${buyer.phone ? `<tr><td class="party-label">Tel:</td><td>${buyer.phone}</td></tr>` : ""}
-                ${buyer.email ? `<tr><td class="party-label">Email:</td><td>${buyer.email}</td></tr>` : ""}
+                <tr><td class="party-label">Účet:</td><td>${buyer.bankAccount}</td></tr>
               </tbody></table>
             </div>
             ` : ""}
-            ${invoice.is_in_eur && buyer.bankAccount ? `
+            ${buyer.phone || buyer.email ? `
             <div class="party-group">
-              <p class="party-group-label">Bankovní spojení:</p>
-              <table class="party-details party-details-indented"><tbody>
-                <tr><td class="party-label">Účet:</td><td>${buyer.bankAccount}</td></tr>
+              <table class="party-details"><tbody>
+                ${buyer.phone ? `<tr><td class="party-label">Tel:</td><td>${buyer.phone}</td></tr>` : ""}
+                ${buyer.email ? `<tr><td class="party-label">Email:</td><td>${buyer.email}</td></tr>` : ""}
               </tbody></table>
             </div>
             ` : ""}
